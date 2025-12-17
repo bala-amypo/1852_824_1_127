4cc62d72365f519b47a07b5c16f3150cd1e10675
bala-amypo <bala@amypo.in>
2025-12-17 05:13:11 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleEchoServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleEchoServlet.java
@@ -1,11 +1,15 @@
 package com.example.demo.servlet;
 
-import.org.springframework.web.annotation.PostMapping
+import.org.springframework.web.annotation.PostMapping;
 @WebServlet
 public class SimpleEchoServlet{
  @PostMapping("/ping")
- public Simple v(){
+ public String v(){
     return "PONG"
+ }
+ @GMapping("/health")
+ public String he(){
+    return "OK";
  }
 
 }
