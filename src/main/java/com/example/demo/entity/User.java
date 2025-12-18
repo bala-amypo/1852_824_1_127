f8ce74fbc5228385470ad3d264c0f4a477863760
bala-amypo <bala@amypo.in>
2025-12-18 05:19:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -17,8 +17,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    public User() {
-    }
+ 
+  
 
     public User(Long id, String name, String email, String password) {
         this.id = id;
@@ -58,4 +58,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    public User(Long id,String name,String email,String password){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+    }
+
+    public User()
+{
+}
 }
