8e0393e7cacf5c3363a48c742d326b9142a3951d
bala-amypo <bala@amypo.in>
2025-12-18 10:19:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplaintStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ComplaintStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ComplaintStatusRepository extends JpaRepository<ComplaintStatus, Long> {
-    List<ComplaintStatus> findByComplaintId(Long complaintId);
-}
