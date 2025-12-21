84207a20be8173a55619dde1ffdb2d7010226705
bala-amypo <bala@amypo.in>
2025-12-21 20:18:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ComplaintController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ComplaintRequest;
+import com.example.demo.entity.Complaint;
+import com.example.demo.service.ComplaintService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/complaints")
+public class ComplaintController {
+
+    private final ComplaintService complaintService;
+
+    public ComplaintController(ComplaintService complaintService) {
+        this.complaintService = complaintService;
+    }
+
+    @PostMapping("/submit")
+    public Complaint submitComplaint(@RequestBody ComplaintRequest request) {
+        return complaintService.submitComplaint(request);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Complaint> getUserComplaints(@PathVariable Long userId) {
+        return complaintService.getUserComplaints(userId);
+    }
+
+    @GetMapping("/prioritized")
+    public List<Complaint> getPrioritizedComplaints() {
+        return complaintService.getPrioritizedComplaints();
+    }
+
+    @PutMapping("/status/{id}")
+    public void updateComplaintStatus(
+            @PathVariable Long id,
+            @RequestParam String status) {
+
+        complaintService.updateComplaintStatus(id, status);
+    }
+}
