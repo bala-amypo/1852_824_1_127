d3acb5fe96223c43a90350e6d00a4951956da6fb
bala-amypo <bala@amypo.in>
2025-12-22 05:18:42 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 import com.example.demo.entity.User;
 public interface UserService {
-    User saveUser(User user);
+    User registerCustomer(String name, String email, String rawPassword);
     User findByEmail(String email);
-    User findById(Long id);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,34 +1,45 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JwtUtil;
 import com.example.demo.service.UserService;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,PasswordEncoder passwordEncoder JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
-    public User saveUser(User user) {
+    public User registerCustomer(String name, String email, String rawPassword) {
+
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("email already exists");
+        }
+
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        user.setRole(Role.CUSTOMER);
+
         return userRepository.save(user);
     }
 
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    @Override
-    public User findById(Long id) {
-        return userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
