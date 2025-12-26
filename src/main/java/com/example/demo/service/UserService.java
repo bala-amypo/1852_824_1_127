3ecaa77a815cec62a52fa396696205366d0892f2
bala-amypo <bala@amypo.in>
2025-12-26 05:13:25 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,45 +1,10 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+public interface UserService {
 
-@Service  
-public class UserServiceImpl implements UserService {
+    User registerCustomer(String name, String email, String rawPassword);
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserServiceImpl(
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder) {
-
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Override
-    public User registerCustomer(String name, String email, String rawPassword) {
-
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new RuntimeException("email already exists");
-        }
-
-        User user = new User();
-        user.setFullName(name);
-        user.setEmail(email);
-        user.setPassword(passwordEncoder.encode(rawPassword));
-        user.setRole(User.Role.CUSTOMER);
-
-        return userRepository.save(user);
-    }
-
-    @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
+    User findByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service  
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -37,7 +39,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByEmail(String email) {
-
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
