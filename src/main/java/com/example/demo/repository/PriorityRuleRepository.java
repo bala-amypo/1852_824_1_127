1146a345ff3a72e5eef2208890cb4576efd9aeeb
bala-amypo <bala@amypo.in>
2025-12-26 04:49:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/PriorityRuleRepository.java
+++ src/main/java/com/example/demo/repository/PriorityRuleRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.PriorityRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+
+import java.util.List;
 
 public interface PriorityRuleRepository extends JpaRepository<PriorityRule, Long> {
-    Optional<PriorityRule> findByCategory(String category);
+
+    List<PriorityRule> findByActiveTrue();
 }
